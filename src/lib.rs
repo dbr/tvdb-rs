c2ac393a445e14985208c41853f484da9cbc9f45
dbr <dbr.onix@gmail.com>
2015-11-20 22:51:21 +1030

    Getting episode name works

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,11 @@ pub struct SeriesSearchResult{
     //pub firstaired: Date,
     pub network: Option<String>,
     pub zap2it_id: Option<String>,
+
+    api: Tvdb,
 }
 
+#[derive(Debug,Clone)]
 pub struct Tvdb{
     key: String,
 }
@@ -91,6 +94,8 @@ impl Tvdb{
                 //firstaired: Date,
                 network:    get_text(child, "Network"),
                 zap2it_id:  get_text(child, "zap2it_id"),
+
+                api: self.clone(), // TODO: Is there better way of allowing EpisodeInfo.episode(..) to access the key?
             };
 
             results.push(r);
@@ -105,14 +110,84 @@ impl Tvdb{
 }
 
 struct EpisodeInfo{
-    pub episodename: String,
+    id: u32, //id
+    // Combined_episodenumber
+    // Combined_season
+    // DVD_chapter
+    // DVD_discid
+    // DVD_episodenumber
+    // DVD_season
+    // Director
+    // EpImgFlag
+    episodename: String, // EpisodeName
+    // EpisodeNumber
+    // FirstAired
+    // GuestStars
+    // IMDB_ID
+    // Language
+    // Overview
+    // ProductionCode
+    // Rating
+    // RatingCount
+    // SeasonNumber
+    // Writer
+    // absolute_number
+    // airsafter_season
+    // airsbefore_episode
+    // airsbefore_season
+    // filename
+    // lastupdated
+    // seasonid
+    // seriesid
+    // thumb_added
+    // thumb_height
+    // thumb_width
 }
 
 impl SeriesSearchResult{
-    fn episode(&self, season: u32, episode: u32) -> EpisodeInfo{
+    fn episode(&self, season: u32, episode: u32) -> Result<EpisodeInfo, TvdbError>{
+        // <mirrorpath>/api/<apikey>/series/{seriesid}/default/{season}/{episode}/{language}.xml
         println!("{} s{}e{}", self.seriesname, season, episode);
 
-        EpisodeInfo{episodename: "My First Day".to_owned()} // FIXME
+        let client = Client::new();
+
+        let formatted_url = format!("http://thetvdb.com/api/{apikey}/series/{seriesid}/default/{season}/{episode}/{language}.xml",
+                                    apikey=self.api.key,
+                                    seriesid=self.seriesid,
+                                    language=self.language,
+                                    season=season,
+                                    episode=episode,
+                                    );
+        let url = Url::parse(&formatted_url).ok().expect("invalid URL");
+        println!("Getting {}", url);
+
+        // Perform request
+        let res = client.get(url)
+            .header(Connection::close())
+            .send();
+
+        let mut res = match res {
+            Err(e) => return Err(TvdbError::CommunicationError{reason: "Error contacting TVDB".to_owned()}), // FIXME: http://stackoverflow.com/questions/28911833/error-handling-best-practices
+            Ok(r) => r
+        };
+
+        // Read the Response.
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        // Parse XML
+        let tree = xmltree::Element::parse(body.as_bytes());
+        let root = tree.children.first().unwrap();
+
+        fn get_text(child: &xmltree::Element, x: &str) -> Option<String>{
+            child.get_child(x).and_then(|id_child| id_child.text.clone())
+        }
+
+        // Convert XML into struct
+        Ok(EpisodeInfo{
+            id: intify(&get_text(root, "id").unwrap()),
+            episodename: get_text(root, "EpisodeName").unwrap(),
+        })
     }
 }
 
@@ -120,34 +195,28 @@ impl SeriesSearchResult{
 mod test{
     use super::*;
 
+    const APIKEY: &'static str = "0629B785CE550C8D";
+
     #[test]
     fn search() {
-        let api = Tvdb::new("APIKEY".to_owned());
+        let api = Tvdb::new(APIKEY.to_owned());
         let sr = api.search("scrubs".to_owned(), "en".to_owned());
         assert!(sr.ok().unwrap()[0].seriesname == "Scrubs");
     }
 
     #[test]
     fn nonexist() {
-        let api = Tvdb::new("APIKEY".to_owned());
+        let api = Tvdb::new(APIKEY.to_owned());
         let sr = api.search("ladlkgdklfgsdfglk".to_owned(), "en".to_owned());
         assert!(sr.is_err());
     }
 
     #[test]
     fn epinfo_default(){
-        let api = Tvdb::new("APIKEY".to_owned());
+        let api = Tvdb::new(APIKEY.to_owned());
         let sr = api.search("scrubs".to_owned(), "en".to_owned()).ok().unwrap();
-        let ep = sr[0].episode(1, 2);
-        assert!(ep.episodename == "My First Day");
-    }
-
-    #[test]
-    fn epinfo_dvd(){
-        let api = Tvdb::new("APIKEY".to_owned());
-        let sr = api.search("scrubs".to_owned(), "en".to_owned()).ok().unwrap();
-        let ep = sr[0].episode(1, 2);
-        assert!(ep.episodename == "My First Day");
+        let ep = sr[0].episode(1, 2).ok().unwrap();
+        assert!(ep.episodename == "My Mentor");
     }
 
 }
